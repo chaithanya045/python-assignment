66acf35ab5efec173af968785c976f613935ab95
chaithanya045 <104122945+chaithanya045@users.noreply.github.com>
2022-05-10 16:13:21 +0530

    Update creatbuc.py

--- python/bucket ops/creatbuc.py
+++ python/bucket ops/creatbuc.py
@@ -1,9 +1,7 @@
-from urllib import response
 import boto3
-s3_client=boto3.client('s3')
-bucket_name=str(input('enter your bucket name:'))
-response=s3_client.create_bucket(
+client=boto3.client('s3')
+bucket_name=str(input('enter your buck name'))
+buck_create=client.create_bucket(
     ACL='private',
     Bucket=bucket_name
 )
-print(response)
